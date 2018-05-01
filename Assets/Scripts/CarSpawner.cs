a213bd3bd506fe5a6b746fac09736dc28729f0dc
Evan Cheng <dr.frolicks@gmail.com>
2018-05-01 14:35:29 -0700

    car spawning rudimentary implementation

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class CarSpawner : MonoBehaviour {
     public GameObject car;
     public float twoCarChance, oneCarChance;
-    public float spawnRowRate;
-    public Transform[] lanePoints; 
-
+    public float spawnIntevalSec;
+    public Transform[] lanePoints;
+    public int spawnOffsetY;
+    private void Start()
+    {
+        spawnWave(); 
+    }
     public void spawnWave()
     {
         float randomNum = Random.value;
@@ -19,6 +23,19 @@ public class CarSpawner : MonoBehaviour {
         openPoints.AddRange(lanePoints);
         ArrayList selectedPoints = new ArrayList(); 
 
+        for(int i = 0; i < numCars; i++)
+        {
+            int pointIndex = Random.Range(0, openPoints.Count);
+            selectedPoints.Add(openPoints[pointIndex]);
+            openPoints.RemoveAt(pointIndex); 
+        }
+
+        foreach(Transform spawnPoint in selectedPoints)
+        {
+            Instantiate(car, spawnPoint.position + Vector3.forward * spawnOffsetY, Quaternion.identity);  
+        }
+
+        Invoke("spawnWave", spawnIntevalSec); 
     }
 
 }
