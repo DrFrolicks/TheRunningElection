6507e9dd3bf5a3f87e771dcbc3b24736130b00a8
Evan Cheng <dr.frolicks@gmail.com>
2018-05-03 10:44:42 -0700

    billboard preset spawning implemented

---
+++ Assets/Scripts/MaterialStartRandom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialStartRandom : MonoBehaviour {
+    public Material[] materials;
+    MeshRenderer meshRenderer;
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();  
+    }
+    // Use this for initialization
+    void Start () {
+        meshRenderer.material = materials[Random.Range(0, materials.Length)]; 
+	}
+
+}
--- Assets/Scripts/PropagandaSurface.cs
+++ Assets/Scripts/PropagandaSurface.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PropagandaSurface : MonoBehaviour {
+    public GameObject display;  
     public int votesAwarded;
     ParticleSystem particleSystem;
-
+    
     bool hasPropaganda = false;  
     private void Awake()
     {
@@ -18,8 +19,9 @@ public class PropagandaSurface : MonoBehaviour {
 
         GameManager.instance.addVotes(votesAwarded);
         particleSystem.Play();
+        display.SetActive(true);
         hasPropaganda = true;
     } 
-    
+
     
 }
